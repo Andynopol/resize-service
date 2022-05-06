f4336d6bcfb3a344d5007e1930d8b174d3b28df9
Andynopol <andrei.nechita95@gmail.com>
2022-05-06 16:07:08 +0300

    Restructure

--- resize-service-tester/resize-service/src/constants/types.ts
+++
@@ -1,7 +0,0 @@
-export type Orientation = "vertical" | "horizontal";
-
-export type OffsetSizeKey = "offsetHeight" | "offsetWidth";
-
-export type SizeKey = "height" | "width";
-
-export type ResizeCases = "exact" | "ratio" | "procentage";
--- resize-service-tester/resize-service/src/utils/scripts.ts
+++
@@ -1,7 +0,0 @@
-export function capitalize ( str: string ) {
-    return `${ str[ 0 ].toUpperCase() }${ str.slice( 1 ) }`;
-};
-
-export function getProcentage ( full: number, x: number ) {
-    return ( 100 * x ) / full;
-};
